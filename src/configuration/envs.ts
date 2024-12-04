93f10a33f4566670edaff7a424e692eeba707258
migue1angel <macf06200@gmail.com>
2024-12-04 08:13:53 -0500

    fix envs config

--- src/configuration/envs.ts
+++ src/configuration/envs.ts
@@ -4,9 +4,6 @@ import 'dotenv/config';
 interface EnvsSchema {
   PORT: number;
   NAT_SERVERS: string[];
-  JWT_SECRET: string;
-  GOOGLE_CLIENT_ID: string;
-  GOOGLE_SECRET: string;
   CLOUD_NAME: string;
   CLOUDINARY_SECRET: string;
   CLOUDINARY_KEY: string;
@@ -26,12 +23,9 @@ export const envsSchema = joi
     PORT: joi.number().required(),
     DB_HOST: joi.string().required(),
     DB_PORT: joi.string().required(),
-    DB_NAME: joi.string().required(),
+    DB_NAME_EVENTS: joi.string().required(),
     DB_PASSWORD: joi.string().required(),
     DB_USER: joi.string().required(),
-    JWT_SECRET: joi.string().required(),
-    GOOGLE_CLIENT_ID: joi.string().required(),
-    GOOGLE_SECRET: joi.string().required(),
     CLOUD_NAME: joi.string().required(),
     CLOUDINARY_SECRET: joi.string().required(),
     CLOUDINARY_KEY: joi.string().required(),
@@ -53,15 +47,11 @@ export const envs: EnvsSchema = {
   database: {
     host: value.DB_HOST,
     port: value.DB_PORT,
-    database: value.DB_NAME,
+    database: value.DB_NAME_EVENTS,
     username: value.DB_USER,
     password: value.DB_PASSWORD,
   },
-
   NAT_SERVERS: value.NAT_SERVERS,
-  JWT_SECRET: value.JWT_SECRET,
-  GOOGLE_CLIENT_ID: value.GOOGLE_CLIENT_ID,
-  GOOGLE_SECRET: value.GOOGLE_SECRET,
   CLOUD_NAME: value.CLOUD_NAME,
   CLOUDINARY_SECRET: value.CLOUDINARY_SECRET,
   CLOUDINARY_KEY: value.CLOUDINARY_KEY,
