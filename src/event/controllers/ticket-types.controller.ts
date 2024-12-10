8456e990636b58057483bbafbae2a814687fd390
migue1angel <macf06200@gmail.com>
2024-12-09 19:29:09 -0500

    fix validate ticket types

--- src/event/controllers/ticket-types.controller.ts
+++ src/event/controllers/ticket-types.controller.ts
@@ -1,43 +1,57 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateTicketTypeDto } from '../dto/ticket_type/create-ticket-type.dto';
 import { UpdateTicketTypeDto } from '../dto/ticket_type/update-ticket-type.dto';
 import { RegistrationsService, TicketTypesService } from '../services';
 import { ValidateTicketTypeDto } from '../dto/ticket_type/validate-ticket-type.dto';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('ticket-types')
 export class TicketTypesController {
-    constructor(private readonly ticketTypesService:TicketTypesService){}
+  constructor(private readonly ticketTypesService: TicketTypesService) {}
 
-    @Post()
-    create(@Body() createTicketTypeDto:CreateTicketTypeDto){
-        return 'This action adds ticket type created';
-    }
+  @Post()
+  create(@Body() createTicketTypeDto: CreateTicketTypeDto) {
+    return 'This action adds ticket type created';
+  }
 
-    @MessagePattern('validateTicketType')
-    async validate(@Body() validateTicketTypeDto:ValidateTicketTypeDto){
-        return await this.ticketTypesService.validateTicketTypes(validateTicketTypeDto);
-    }
+  @MessagePattern('validateTicketTypes')
+  async validate(@Payload() validateTicketTypeDto: ValidateTicketTypeDto[]) {
+    const ticketTypes = await this.ticketTypesService.validateTicketTypes(
+      validateTicketTypeDto,
+    );
+    return ticketTypes;
+  }
+ 
+  @Get()
+  findAll() {
+    return 'This action returns ticket types Found';
+  }
 
-    @Get()
-    findAll(){
-        return 'This action returns ticket types Found';
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a ${id} ticket type`;
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string){
-        return `This action returns a ${id} ticket type`;
-    }
+  @Patch()
+  async update(
+    @Param('id') id: string,
+    @Body() updateTicketTypeDto: UpdateTicketTypeDto,
+  ) {
+    await this.ticketTypesService.update(id, updateTicketTypeDto);
 
-    @Patch()
-    async update(@Param('id') id:string, @Body() updateTicketTypeDto:UpdateTicketTypeDto){
-        await this.ticketTypesService.update(id, updateTicketTypeDto)
+    return `This action update a ${id} ticket type`;
+  }
 
-        return `This action update a ${id} ticket type`
-    }
-
-    @Delete(':id')
-    delete(@Param('id')id:string){
-        return `This action remove a ${id} ticket type`
-    }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `This action remove a ${id} ticket type`;
+  }
 }
